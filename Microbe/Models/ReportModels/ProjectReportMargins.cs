61c78830dbe0473673bfad2d5adaf7463a1680ef
Bob Boyle <bob@boylecs.com>
2018-11-08 08:41:17 -0500

    ReportMargins

---
+++ Microbe/Models/ReportModels/ProjectReportMargins.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microbe.Models.ReportModels
+{
+    public class ProjectReportMargins
+    {
+        public int ProjectReportMarginsID { get; set; }
+        public string ProjectID { get; set; }
+        public float MarginRight { get; set; }
+        public float MarginLeft { get; set; }
+        public float MarginTop { get; set; }
+        public float MarginBottom { get; set; }
+        public string FooterTxt { get; set; }
+        public string FirstPageFooterTxt { get; set; }
+        public string HeaderTxt { get; set; }
+        public string FirstPageHeaderTxt { get; set; }
+    }
+}
---
+++ Microbe/Models/ReportModels/ProjectReportSections.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microbe.Models.ReportModels
+{
+    public class ProjectReportSections
+    {
+        public int ProjectReportSectionsID { get; set; }
+        public string ProjectID { get; set; }
+        public string ReportType { get; set; }
+        public int ReportSectionID { get; set; }
+        public int ParentProjectReportSectionID { get; set; }
+        public string PRS_Description { get; set; }
+    }
+}
